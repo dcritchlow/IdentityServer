154e4d6be9ed6315833843574c8e4dc13f013e0e
Darin Critchlow <darincritchlow@gmail.com>
2020-02-03 01:07:13 -0700

    Identity Server with AspNetIdentity

--- quickstart/src/IdentityServerAspNetIdentity/Config.cs
+++ quickstart/src/IdentityServerAspNetIdentity/Config.cs
@@ -6,27 +6,28 @@ using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
-namespace IdentityServer
+namespace IdentityServerAspNetIdentity
 {
     public static class Config
     {
         public static IEnumerable<IdentityResource> Ids =>
-            new List<IdentityResource>
+            new IdentityResource[]
             {
                 new IdentityResources.OpenId(),
                 new IdentityResources.Profile(),
             };
 
+
         public static IEnumerable<ApiResource> Apis =>
-            new List<ApiResource>
+            new ApiResource[]
             {
-                new ApiResource("api1", "My API")
+                new ApiResource("api1", "My API #1")
             };
-        
+
+
         public static IEnumerable<Client> Clients =>
             new List<Client>
             {
-                // machine to machine client
                 new Client
                 {
                     ClientId = "client",
@@ -43,27 +44,40 @@ namespace IdentityServer
                     // scopes that client has access to
                     AllowedScopes = { "api1" }
                 },
+                // resource owner password grant client
+                new Client
+                {
+                    ClientId = "ro.client",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "api1" }
+                },
                 // interactive ASP.NET Core MVC client
                 new Client
                 {
                     ClientId = "mvc",
                     ClientName = "MVC Client",
-                    ClientSecrets = { new Secret("secret".Sha256()) },
-
                     AllowedGrantTypes = GrantTypes.Code,
-                    RequireConsent = false,
                     RequirePkce = true,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
 
-                    // where to redirect to after login
-                    RedirectUris = { "http://localhost:5002/signin-oidc" },
-
-                    // where to redirect to after logout
+                    RedirectUris           = { "http://localhost:5002/signin-oidc" },
                     PostLogoutRedirectUris = { "http://localhost:5002/signout-callback-oidc" },
 
-                    AllowedScopes = new List<string>
+                    AllowedScopes =
                     {
                         IdentityServerConstants.StandardScopes.OpenId,
                         IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        IdentityServerConstants.StandardScopes.Address,
+                        IdentityServerConstants.StandardScopes.OfflineAccess,
                         "api1"
                     },
 
@@ -90,6 +104,5 @@ namespace IdentityServer
                     }
                 }
             };
-        
     }
 }
